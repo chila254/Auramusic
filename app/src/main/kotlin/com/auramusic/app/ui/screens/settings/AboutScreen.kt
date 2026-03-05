923807ea0981859cbf9376f09efae470069b8091
chila254 <franklinfinyange@gmail.com>
2026-03-06 01:54:33 +0300

    Fix BlendMode import in AboutScreen.kt

--- app/src/main/kotlin/com/auramusic/app/ui/screens/settings/AboutScreen.kt
+++ app/src/main/kotlin/com/auramusic/app/ui/screens/settings/AboutScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
